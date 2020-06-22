3c86f9d0373f649eabc942f3b06e90246fbf9584
Galymzhan Aigelov <galymzhan@otgroup.kz>
2020-06-23 00:09:06 +0600

    Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import {createStore, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux'
+import reduxThunk from 'redux-thunk'
+import rootReducer from './redux/rootReducer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function loggerMiddleware(store) {
+//   return function(next) {
+//     return function(action) {
+//       const result = next(action)
+//       console.log('Middleware', store.getState())
+//       return result
+//     }
+//   }
+// }
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const loggerMiddleware = store => next => action => {
+  const result = next(action)
+  console.log('Middleware', store.getState())
+  return result
+}
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
+  loggerMiddleware,
+  reduxThunk
+)))
+
+const app = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
